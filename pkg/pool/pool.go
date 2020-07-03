47fa61611070490a759e56d39c34315028c23217
Eric Wohltman <eric.wohltman@gmail.com>
2020-07-03 08:11:46 -0400

    Tighten up pool.UnmarshalObject function

--- pkg/pool/pool.go
+++ pkg/pool/pool.go
@@ -11,13 +11,6 @@ import (
 )
 
 // nolint:gochecknoglobals // package global variable to simplify example
-var bufferPool = sync.Pool{
-	New: func() interface{} {
-		return new(bytes.Buffer)
-	},
-}
-
-// nolint:gochecknoglobals // package global variable to simplify example
 var objectPool = sync.Pool{
 	New: func() interface{} {
 		return &base.Object{
@@ -36,26 +29,30 @@ var contentPool = sync.Pool{
 	},
 }
 
+// nolint:gochecknoglobals // package global variable to simplify example
+var bufferPool = sync.Pool{
+	New: func() interface{} {
+		return new(bytes.Buffer)
+	},
+}
+
 // UnmarshalObject writes jsonString into a *bytes.Buffer using sync.Pool and
 // unmarshals the buffer into a *base.Object. It then returns the dereferenced
 // base.Object. The *base.Object is also obtained using sync.Pool.
 func UnmarshalObject(jsonString string) (base.Object, error) {
-	buffer := bufferPool.Get().(*bytes.Buffer)
-
-	defer bufferPool.Put(buffer)
-	defer buffer.Reset()
-
 	object := objectPool.Get().(*base.Object)
-
 	defer objectPool.Put(object)
 	defer object.Reset()
 
 	contents := contentPool.Get().(*base.Contents)
-
 	defer contentPool.Put(contents)
 	defer contents.Reset()
 
 	object.Contents = contents
+
+	buffer := bufferPool.Get().(*bytes.Buffer)
+	defer bufferPool.Put(buffer)
+	defer buffer.Reset()
 
 	buffer.WriteString(jsonString)
 
